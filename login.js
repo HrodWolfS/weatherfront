188b51fcd17a4b432c170ec4229559dd59e2c352
HrodWolfDev <102832449+HrodWolfS@users.noreply.github.com>
2025-11-02 10:06:59 +0100

    exo finis sans bonus

---
+++ login.js
@@ -0,0 +1,44 @@
+// Insert your code here
+const registerName = document.querySelector("#registerName");
+const registerEmail = document.querySelector("#registerEmail");
+const registerPassword = document.querySelector("#registerPassword");
+
+document.querySelector("#register").addEventListener("click", () => {
+  console.log(registerName.value, registerEmail.value, registerPassword.value);
+  fetch("http://localhost:3000/users/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: registerName.value,
+      email: registerEmail.value,
+      password: registerPassword.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.result) {
+        window.location.assign("index.html");
+      }
+    });
+});
+
+const connectionEmail = document.querySelector("#connectionEmail");
+const connectionPassword = document.querySelector("#connectionPassword");
+
+document.querySelector("#connection").addEventListener("click", () => {
+  console.log(connectionEmail.value, connectionPassword.value);
+  fetch("http://localhost:3000/users/signin", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: connectionEmail.value,
+      password: connectionPassword.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.result) {
+        window.location.assign("index.html");
+      }
+    });
+});
